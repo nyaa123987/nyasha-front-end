825be934d97541720236749eceb466c9de87841e
nyasha <nyashazee07@gmail.com>
2025-08-20 14:30:06 +0200

    Initial commit

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -6,11 +6,11 @@ export default function Skills() {
   return (
     <section className="max-w-7xl mx-auto px-8 py-16 md:py-40">
       <H2>Skills & Tools</H2>
-      <div className="flex flex-wrap justify-center gap-10">
+      <div className="flex flex-wrap justify-center gap-2 md:gap-6">
         {skills.map((skill) => (
           <div
             key={skill}
-            className="px-4 py-2 md:px-6 md:py-3 rounded-xl md:rounded-full bg-[#1d0b55] flex items-center justify-center md:font-semibold text-[14px] md:text-xl shadow-lg text-white"
+            className="px-4 py-2 md:px-6 md:py-3 rounded-xl md:rounded-full bg-[#1d0b55] flex items-center justify-center md:font-semibold text-sm md:text-lg shadow-lg text-white"
           >
             {skill}
           </div>
